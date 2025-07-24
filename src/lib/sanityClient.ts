7cb301ef0d35572dae1e4bd4e9ace441ade23e18
Sachin Rathnayake <sachirathnayake9@gmail.com>
2025-07-24 13:31:10 +0530

    feat: Integrate Sanity.io client for dynamic content

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -6,5 +6,5 @@ export const client = createClient({
   apiVersion: '2025-07-15',
   useCdn: false,
   // Add the token you just copied here
-  token: 'sklZYZXEzx9xzfOMGcRxhuguy0GmC0afws7Nf5jn1ZzZ7ls3zEdIGX1hJS6ImZszbU4geNft6dczoTM3LQwopiqzpW98TLj8TkkAKFEfzlPRFC0WPsrMe2ZS36hxow5EjJHF0DZaAsHvP1msvKf6cud1ehvUf2nOTgLwWQWFSx7t5P73GSGr', // <-- ඔබ COPY කරගත්තු TOKEN එක මෙතනට PASTE කරන්න
+  token: process.env.SANITY_API_WRITE_TOKEN,
 });
